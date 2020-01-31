3772d17def3e4955b560c514a224c75426e3a1e2
Mauricio Saez Toro <Mauricio.saez.toro@skolverket.se>
2020-01-31 13:46:35 +0000

    test3 (jenkins pipeline)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "What? A Flask application in a Docker container!"
+    return "What? A Flask application in a Docker container! \o/"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
